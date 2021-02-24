d18ad5a64dc631945f63651832b15f8860a58b31
eyrc-vb-1244 <79420807+eyrc-vb-1244@users.noreply.github.com>
2021-02-24 17:05:16 +0530

    Update map.js

--- map.js
+++ map.js
@@ -45,24 +45,24 @@ function refreshMap(){
   }
   
   var greenIcon = new L.Icon({
-    iconUrl: 'img/marker-icon-2x-green.png',
-	shadowUrl: 'img/marker-shadow.png',
+    iconUrl: 'marker-icon-2x-green.png',
+	shadowUrl: 'marker-shadow.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
     shadowSize: [41, 41]
   });
   var redIcon = new L.Icon({
-    iconUrl: 'img/marker-icon-2x-red.png',
-	shadowUrl: 'img/marker-shadow.png',
+    iconUrl: 'marker-icon-2x-red.png',
+	shadowUrl: 'marker-shadow.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
     shadowSize: [41, 41]
   });
   var goldIcon = new L.Icon({
-    iconUrl: 'img/marker-icon-2x-gold.png',
-	shadowUrl: 'img/marker-shadow.png',
+    iconUrl: 'marker-icon-2x-gold.png',
+	shadowUrl: 'marker-shadow.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
